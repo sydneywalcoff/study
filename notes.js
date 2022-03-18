31b5148b927e41eb48aa7d24a86a9d15f7cce9d6
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-18 13:45:33 -0700

    intial algos notes; euclids algorith -- finding greatest common denominator

---
+++ notes.js
@@ -0,0 +1,22 @@
+// search algorithms
+
+// sorting algorithms
+
+// computational algorithms
+
+// collection algorithms
+
+
+// Euclid's Algorithm
+const findGCD = (a,b) => {
+    while(b != 0) {
+        const rem = a % b;
+        a = b;
+        b = rem;
+        if(rem == 0) return a;
+    }
+};
+
+
+console.log(findGCD(20, 8)); /* should be 4 */
+console.log(findGCD(60, 96)); /* should be 4 */

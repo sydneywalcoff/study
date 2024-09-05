ce0d4bea8e0ef6f2f2fcdb5aa8e18466fc658c19
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-04 21:58:45 -0700

    list pop method

--- python/lessons/list-methods.py
+++ python/lessons/list-methods.py
@@ -2,4 +2,11 @@
 front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
 
 front_display_list.insert(0, 'Pineapple')
-print(front_display_list)
+# print(front_display_list)
+
+# Pop
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+
+data_science_topics.pop()
+data_science_topics.pop(3)
+# print(data_science_topics)

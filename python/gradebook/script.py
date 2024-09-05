bf8b7d5277ccd6a5b13e09b0d2936fe11d395e58
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-04 21:43:57 -0700

    gradebook assignment on codecademy

---
+++ python/gradebook/script.py
@@ -0,0 +1,20 @@
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+
+subjects = ["physics", "calculus", "poetry", "history"]
+
+grades = [98, 97, 85, 88]
+
+gradebook = [["physics", 98], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+gradebook.append(['computer science', 100])
+
+gradebook.append(["visual arts", 93])
+
+gradebook[5][1] += 5
+
+gradebook[2].remove(85)
+gradebook[2].append('pass')
+
+full_gradebook = gradebook + last_semester_gradebook
+
+print(full_gradebook)

269a6368db636c1a55e1fee4141f8dc54ccc79d2
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-05 10:25:33 -0700

    counting

--- python/lessons/list-methods.py
+++ python/lessons/list-methods.py
@@ -39,3 +39,8 @@ middle = suitcase[2:4]
 last_two_elements = suitcase[-2:]
 slice_off_last_three = suitcase[:-3]
 # print(slice_off_last_three)
+
+# counting
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+jake_votes = votes.count('Jake')
+# print(jake_votes)

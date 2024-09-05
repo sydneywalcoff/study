532464b1dd063e21c0a176134b29cb6a9049e4ca
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-05 10:15:06 -0700

    length methods

--- python/lessons/list-methods.py
+++ python/lessons/list-methods.py
@@ -19,3 +19,13 @@ zero_to_seven = range(8)
 range_five_three = range(5, 15, 3)
 range_diff_five = range(0, 40, 5)
 # print(list(range_diff_five))
+
+# length
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+
+big_range = range(2, 3000, 10)
+
+long_list_len = len(long_list)
+
+big_range_length = len(big_range)
+# print(big_range_length)

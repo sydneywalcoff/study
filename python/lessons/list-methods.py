e039abebe4cd70f02e6b68a204bc4ec9aefe5d87
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-04 22:07:44 -0700

    range: skip sinput

--- python/lessons/list-methods.py
+++ python/lessons/list-methods.py
@@ -11,8 +11,11 @@ data_science_topics.pop()
 data_science_topics.pop(3)
 # print(data_science_topics)
 
-# Consecutive Lists: Range
+# Range
 number_list = range(9)
-
 zero_to_seven = range(8)
 # print(list(zero_to_seven))
+
+range_five_three = range(5, 15, 3)
+range_diff_five = range(0, 40, 5)
+# print(list(range_diff_five))

cfb99d5dc325a35dda6f1cad02f42d8e6a64666c
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-06 20:50:05 -0700

    sorted fxn

--- python/lessons/list-methods.py
+++ python/lessons/list-methods.py
@@ -45,7 +45,7 @@ votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "
 jake_votes = votes.count('Jake')
 # print(jake_votes)
 
-#sorting 
+# sorting 
 addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
 addresses.sort()
 # print(addresses)
@@ -57,3 +57,9 @@ names.sort()
 cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
 cities.sort(reverse=True)
 # print(cities)
+
+# sorted function
+games = ["Portal", "Minecraft", "Pacman", "Tetris", "The Sims", "Pokemon"]
+
+games_sorted = sorted(games)
+# print(games, games_sorted)

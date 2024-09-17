c7e3f77cf43cdee7a98c5af84a2ffd010b3f9d7f
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-17 14:48:18 -0700

    using range w/ for loops

--- python/lessons/for-loop.py
+++ python/lessons/for-loop.py
@@ -1,9 +1,17 @@
+# for loops
 board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
 
 sport_games = ["football", "hockey", "baseball", "cricket"]
 
-for game in board_games:
-  print(game)
+# for game in board_games:
+#   print(game)
 
-for sport in sport_games:
-  print(sport)
+# for sport in sport_games:
+#   print(sport)
+
+# for loops: ranges
+promise = "I will finish the python loops module!"
+
+# for i in range(5):
+#     print(promise)
+

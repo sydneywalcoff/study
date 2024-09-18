293e83f4bbac23b7a3ee553eedaaeb580f943da3
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-18 14:14:35 -0700

    while loop over lists

--- python/lessons/while-loop.py
+++ python/lessons/while-loop.py
@@ -1,6 +1,16 @@
 countdown = 10
-while countdown >= 0:
-  print(countdown)
-  countdown -= 1
+# while countdown >= 0:
+#   print(countdown)
+#   countdown -= 1
 
-print("We have liftoff!")
+# print("We have liftoff!")
+
+# list
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+length = len(python_topics)
+index = 0
+
+while index < length:
+    print("I am learning about " + python_topics[index])
+    index+= 1

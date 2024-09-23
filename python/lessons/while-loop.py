534e605b1eff4b66080b3f762ea97ae254f150b7
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-23 10:43:17 -0700

    while loop: break

--- python/lessons/while-loop.py
+++ python/lessons/while-loop.py
@@ -11,6 +11,17 @@ python_topics = ["variables", "control flow", "loops", "modules", "classes"]
 length = len(python_topics)
 index = 0
 
-while index < length:
-    print("I am learning about " + python_topics[index])
-    index+= 1
+# while index < length:
+#     print("I am learning about " + python_topics[index])
+#     index+= 1
+
+
+# loop control: break
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+    print(dog_breed)
+    if dog_breed == dog_breed_I_want:
+        break
+print("They have the dog I want!")

b8aef9cb0fdbe006bf26e7961a5aeb617eaa4bb7
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-23 13:19:05 -0700

    added more to dictionaries lessons

--- python/lessons/dictionaries.py
+++ python/lessons/dictionaries.py
@@ -2,5 +2,34 @@
 sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 num_cameras = {"backyard": 6, "garage": 2, "driveway": 1}
 
-print(sensors)
+# print(sensors)
+
+# creating dictionaries
+translations = {
+    "mountain": "orod",
+    "bread": "bass",
+    "friend": "mellon",
+    "horse": "roch"
+}
+
+# adding key/value pairs
+animals_in_zoo = {}
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+# print(animals_in_zoo)
+
+# add multiple keys - .update() method
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+# print(user_ids)
+
+# overwrite values
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners["Supporting Actress"] = "Viola Davis"
+oscar_winners["Best Picture"] = "Moonlight"
+
+# dict comprehensions
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
 

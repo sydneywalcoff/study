06459b2fbb3443944f729e6ea2d9b3e173d9e2d5
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-25 15:22:15 -0700

    get and pop dictionary methods

--- python/lessons/dictionaries.py
+++ python/lessons/dictionaries.py
@@ -57,4 +57,19 @@ zodiac_elements = {"water": ["Cancer", "Scorpio", "Pisces"], "fire": ["Aries", "
 
 # get an invalid key
 zodiac_elements["energy"] = "Not a Zodiac element"
-if("energy" in zodiac_elements): print(zodiac_elements["energy"])
+# if("energy" in zodiac_elements): print(zodiac_elements["energy"])
+
+# safely get a key
+user_ids = {"teraCoder": 100019, "pythonGuy": 182921, "samTheJavaMaam": 123112, "lyleLoop": 102931, "keysmithKeith": 129384}
+tc_id = user_ids.get('teraCoder', 100000)
+stack_id = user_ids.get("superStackSmash", 100000)
+# print(stack_id)
+
+# delete a key
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+health_points += available_items.pop('stamina grains', 0)
+health_points += available_items.pop('power stew', 0)
+health_points += available_items.pop("mystic bread", 0)
+# print(available_items, health_points)
+

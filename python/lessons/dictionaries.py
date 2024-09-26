f5cf962c8693af86fa668b710658ff72605c18de
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-26 15:14:00 -0700

    get all items and review for dictionaries

--- python/lessons/dictionaries.py
+++ python/lessons/dictionaries.py
@@ -88,3 +88,18 @@ for ex in num_exercises.values():
   total_exercises += ex
 # print(total_exercises)
 
+# get all items
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+# for occupation, percent in pct_women_in_occupation.items():
+#     print("Women make up "+ str(percent) + " percent of "+ occupation + "s.")
+
+# review 
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+spread = {}
+spread["past"] = tarot.pop(13)
+spread["present"] = tarot.pop(22)
+spread["future"] = tarot.pop(10)
+
+for key,value in spread.items():
+  print("Your "+ key + " is the "+ value+" card.")

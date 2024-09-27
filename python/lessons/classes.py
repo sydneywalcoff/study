4d97dd746f9a4582d54cabe78e6dd83a4219a754
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-27 16:08:27 -0700

    methods, methods w/ arguments, constructors

--- python/lessons/classes.py
+++ python/lessons/classes.py
@@ -17,3 +17,31 @@ facade_1_type = type(facade_1)
 # class variables
 class Grade:
     minimum_passing = 65
+
+# methods
+class Rules:
+    def washing_brushes(self):
+        return "Point bristles towards the basin while washing your brushes."
+
+# methods with arguments
+class Circle:
+    pi = 3.14
+    
+    def area(self, radius):
+        return self.pi * radius ** 2
+
+circle = Circle()
+pizza_area = circle.area((12/2))
+teaching_table_area = circle.area((36/2))
+round_room_area = circle.area((11460/2))
+
+# constructors
+# dunder methods - double underscore on either side. ex) __init__()
+
+class Circle1:
+  pi = 3.14
+  def __init__(self, diameter):
+    print("New circle with diameter: "+ str(diameter))
+
+# teaching_table = Circle(36) # prints New circle with diameter: 36
+    

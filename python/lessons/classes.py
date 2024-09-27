577cb200bc92217e6040096758c4c8c64501f44f
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-09-27 15:47:19 -0700

    instantiation

--- python/lessons/classes.py
+++ python/lessons/classes.py
@@ -5,3 +5,11 @@ my_dict = {}
 my_list = []
 # print(type(my_list)) # list
 
+# instantiation
+class Facade:
+  pass # pass tells compiler that we want empty class
+
+facade_1 = Facade() # instantiation
+
+facade_1_type = type(facade_1)
+print(facade_1_type) # <class '__main__.Facade'>
